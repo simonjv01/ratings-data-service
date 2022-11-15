9c0dfef1cae4aefff2614b2cc2f84f791dae72b8
simonjv01 <sjvargas01@gmail.com>
2022-11-14 20:07:39 -0500

    changed name of url

--- src/main/java/com/example/ratingsdataservice/resources/RatingsResource.java
+++ src/main/java/com/example/ratingsdataservice/resources/RatingsResource.java
@@ -11,7 +11,7 @@ import java.util.Arrays;
 import java.util.List;
 
 @RestController
-@RequestMapping("/ratingsdata")
+@RequestMapping("/ratings-data")
 public class RatingsResource {
 
     @RequestMapping("/{movieId}")
